af3e3b832249a1f96e751c1a98076e9571be4506
chobolevel <rodaka123@naver.com>
2024-01-26 23:16:47 +0900

    Feat: add redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import rootReducer from './reducer'
+import { createStore } from 'redux'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const store = createStore(rootReducer)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+)
