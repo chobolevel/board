4865deada66bf53daffcfcae38f7acc0eb7d59c5
chobolevel <rodaka123@naver.com>
2024-01-26 23:28:04 +0900

    Feat: add base data

---
+++ src/data/index.js
@@ -0,0 +1,218 @@
+export const users = [
+  {
+    id: 1,
+    username: 'user',
+    password: '1234',
+    name: '일반 사용자',
+    role: 'ROLE_USER',
+  },
+  {
+    id: 2,
+    username: 'business',
+    password: '1234',
+    name: '사업자 사용자',
+    role: 'ROLE_BUSINESS',
+  },
+]
+export const boards = [
+  {
+    id: 1,
+    title: '테스트 게시글 제목1',
+    content: '테스트 게시글 내용1',
+    writerId: 1,
+    comments: [
+      {
+        id: 1,
+        title: '테스트 댓글 제목1',
+        content: '테스트 댓글 내용1',
+        writerId: 1,
+      },
+      {
+        id: 2,
+        title: '테스트 댓글 제목2',
+        content: '테스트 댓글 내용2',
+        writerId: 2,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: '테스트 게시글 제목2',
+    content: '테스트 게시글 내용2',
+    writerId: 1,
+    comments: [
+      {
+        id: 1,
+        title: '테스트 댓글 제목1',
+        content: '테스트 댓글 내용1',
+        writerId: 1,
+      },
+      {
+        id: 2,
+        title: '테스트 댓글 제목2',
+        content: '테스트 댓글 내용2',
+        writerId: 2,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: '테스트 게시글 제목3',
+    content: '테스트 게시글 내용3',
+    writerId: 1,
+    comments: [
+      {
+        id: 1,
+        title: '테스트 댓글 제목1',
+        content: '테스트 댓글 내용1',
+        writerId: 1,
+      },
+      {
+        id: 2,
+        title: '테스트 댓글 제목2',
+        content: '테스트 댓글 내용2',
+        writerId: 2,
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: '테스트 게시글 제목4',
+    content: '테스트 게시글 내용4',
+    writerId: 1,
+    comments: [
+      {
+        id: 1,
+        title: '테스트 댓글 제목1',
+        content: '테스트 댓글 내용1',
+        writerId: 1,
+      },
+      {
+        id: 2,
+        title: '테스트 댓글 제목2',
+        content: '테스트 댓글 내용2',
+        writerId: 2,
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: '테스트 게시글 제목5',
+    content: '테스트 게시글 내용5',
+    writerId: 1,
+    comments: [
+      {
+        id: 1,
+        title: '테스트 댓글 제목1',
+        content: '테스트 댓글 내용1',
+        writerId: 1,
+      },
+      {
+        id: 2,
+        title: '테스트 댓글 제목2',
+        content: '테스트 댓글 내용2',
+        writerId: 2,
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: '테스트 게시글 제목6',
+    content: '테스트 게시글 내용6',
+    writerId: 1,
+    comments: [
+      {
+        id: 1,
+        title: '테스트 댓글 제목1',
+        content: '테스트 댓글 내용1',
+        writerId: 1,
+      },
+      {
+        id: 2,
+        title: '테스트 댓글 제목2',
+        content: '테스트 댓글 내용2',
+        writerId: 2,
+      },
+    ],
+  },
+  {
+    id: 7,
+    title: '테스트 게시글 제목7',
+    content: '테스트 게시글 내용7',
+    writerId: 1,
+    comments: [
+      {
+        id: 1,
+        title: '테스트 댓글 제목1',
+        content: '테스트 댓글 내용1',
+        writerId: 1,
+      },
+      {
+        id: 2,
+        title: '테스트 댓글 제목2',
+        content: '테스트 댓글 내용2',
+        writerId: 2,
+      },
+    ],
+  },
+  {
+    id: 8,
+    title: '테스트 게시글 제목8',
+    content: '테스트 게시글 내용8',
+    writerId: 1,
+    comments: [
+      {
+        id: 1,
+        title: '테스트 댓글 제목1',
+        content: '테스트 댓글 내용1',
+        writerId: 1,
+      },
+      {
+        id: 2,
+        title: '테스트 댓글 제목2',
+        content: '테스트 댓글 내용2',
+        writerId: 2,
+      },
+    ],
+  },
+  {
+    id: 9,
+    title: '테스트 게시글 제목9',
+    content: '테스트 게시글 내용9',
+    writerId: 1,
+    comments: [
+      {
+        id: 1,
+        title: '테스트 댓글 제목1',
+        content: '테스트 댓글 내용1',
+        writerId: 1,
+      },
+      {
+        id: 2,
+        title: '테스트 댓글 제목2',
+        content: '테스트 댓글 내용2',
+        writerId: 2,
+      },
+    ],
+  },
+  {
+    id: 10,
+    title: '테스트 게시글 제목10',
+    content: '테스트 게시글 내용10',
+    writerId: 1,
+    comments: [
+      {
+        id: 1,
+        title: '테스트 댓글 제목1',
+        content: '테스트 댓글 내용1',
+        writerId: 1,
+      },
+      {
+        id: 2,
+        title: '테스트 댓글 제목2',
+        content: '테스트 댓글 내용2',
+        writerId: 2,
+      },
+    ],
+  },
+]
